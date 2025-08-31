3b3b0d7234b8f3d9d702e04adb284b813ab3871b
luyosamson <luyosamson@gmail.com>
2025-08-31 13:50:21 +0300

    Fxn in JavaScripts

---
+++ functions/JavaScriptFunctions/calc.js
@@ -0,0 +1,12 @@
+const pie=3.142
+var num1=10
+
+function add(n1,n2){
+    let num1=20;
+    let num2=30;
+    let result=num1+num2
+    console.log(num1)
+    console.log(result)
+}
+
+console.log(add())
