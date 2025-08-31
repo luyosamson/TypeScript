d4ecadbee2cd4a4e7f170102af04836c67e3f7dd
luyosamson <luyosamson@gmail.com>
2025-08-31 14:17:31 +0300

    Function Expression

--- functions/JavaScriptFunctions/calc.js
+++ functions/JavaScriptFunctions/calc.js
@@ -4,10 +4,17 @@ var num1=10
 function add(n1,n2){
     let num1=200;
     let num2=300;
-    let result=num1+num2
+    // let result=num1+num2
     console.log(num1)
 
-    return result
+    return n1+n2
 }
 
-console.log(add())
+console.log(add(100,900))
+
+//Function Expression
+
+const product=function(a,b){
+    return a*b
+}
+console.log(product(12,12))
