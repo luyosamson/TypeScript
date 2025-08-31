7fefcf5da56841bc9a4277cabfd7e0806ed5c9ea
luyosamson <luyosamson@gmail.com>
2025-08-31 13:56:16 +0300

    Modified

--- functions/JavaScriptFunctions/calc.js
+++ functions/JavaScriptFunctions/calc.js
@@ -2,11 +2,12 @@ const pie=3.142
 var num1=10
 
 function add(n1,n2){
-    let num1=20;
-    let num2=30;
+    let num1=200;
+    let num2=300;
     let result=num1+num2
     console.log(num1)
-    console.log(result)
+
+    return result
 }
 
 console.log(add())
